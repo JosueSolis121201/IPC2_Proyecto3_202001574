395d4548fda6b1abddfefb8b6c625b34d57f5a57
JosueSolis121201 <89563610+JosueSolis121201@users.noreply.github.com>
2022-05-04 17:19:53 -0600

    html

--- Proyecto_3/Proyecto_3/views.py
+++ Proyecto_3/Proyecto_3/views.py
@@ -1,5 +1,14 @@
+from re import template
+from unittest import loader
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+import requests
 
 def home (request):
-    return render(request,"Pagina.html")
+    template =loader.get_template("pagina.html")
+
+    respuesta = requests.get("http://127.0.0.1:5000/estudiante")
+    context= respuesta.json()
+    return HttpResponse(template.render(context, request))
     
